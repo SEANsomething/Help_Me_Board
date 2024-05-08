4e5286aacbf15ffed6f51e449482e6d0f350e829
이창규 <ichang-gyu@ichang-gyus-MacBook-Pro.local>
2024-05-08 19:51:26 +0900

    madal 추가

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,16 @@
 import Header from "../../components/Header";
+import Modal from "../../components/Modal";
 
 export default function Home() {
 	return <>
 		<Header />
-		<h1>Home Page</h1>
+		<Modal />
+		<div id="login_popup">
+			<input type="ID를 입력하세요" />
+			<input type="PW를 입력하세요" />
+
+			<button>로그인</button>
+			<button>회원가입</button>
+		</div>
 	</>
 }
