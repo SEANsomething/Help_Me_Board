aee943cd1978fbff96fa50cf79d3342c7c5df267
SEANsomething <devseanchu@gmail.com>
2024-05-22 18:52:53 +0900

    PostsView 생성

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,19 +1,19 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "pages/home";
 import PostNew from "pages/posts/index";
+import PostView from "pages/posts/view";
 import PostEdit from "pages/posts/edit";
 import Profile from "pages/profile";
 import SignUp from "pages/signUp";
-import Comment from "pages/comment";
 
 export default function RouterPage() {
 	return <Routes>
 		<Route path='/' element={<Home />}/>
 		<Route path='/post/new' element={<PostNew />}/>
+		<Route path='/post/:id' element={<PostView />}/>
 		<Route path='/post/edit/:id' element={<PostEdit />}/>
 		<Route path='/profile' element={<Profile />}/>
 		<Route path='/signUp' element={<SignUp />}/>
-		<Route path='/comment' element={<Comment />}/>
 		<Route path="*" element={<Navigate replace to="/" />}/>
 	</Routes>
 }
---
+++ src/pages/posts/view.jsx
@@ -0,0 +1,8 @@
+import Header from "components/Header";
+
+export default function PostView() {
+	return <>
+		<Header />
+		<h1>Post View</h1>
+	</>;
+}
