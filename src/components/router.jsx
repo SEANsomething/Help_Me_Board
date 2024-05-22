ea151b5907a9565939e7f380a3df14f56858ca43
이창규 <ichang-gyu@ichang-gyus-MacBook-Pro.local>
2024-05-22 18:27:22 +0900

    댓글

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -4,6 +4,7 @@ import PostNew from "pages/posts/index";
 import PostEdit from "pages/posts/edit";
 import Profile from "pages/profile";
 import SignUp from "pages/signUp";
+import Comment from "pages/comment";
 
 export default function RouterPage() {
 	return <Routes>
@@ -12,6 +13,7 @@ export default function RouterPage() {
 		<Route path='/post/edit/:id' element={<PostEdit />}/>
 		<Route path='/profile' element={<Profile />}/>
 		<Route path='/signUp' element={<SignUp />}/>
+		<Route path='/comment' element={<Comment />}/>
 		<Route path="*" element={<Navigate replace to="/" />}/>
 	</Routes>
 }
