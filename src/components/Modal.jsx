38db428eae443fb54a6929fb82a61ddb710b2c52
이창규 <ichang-gyu@ichang-gyus-MacBook-Pro.local>
2024-05-29 18:42:37 +0900

    로그인팝업 뼈대작업

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 export default function Modal() {
     return <div className="modal">
+        <div className="modal__popup">
 			<div className="modal__input">
 				<input className="modal__input--id" type ="text" placeholder="ID를 입력하세요" />
 				<input className="modal__input--pw" type="text" placeholder="PW를 입력하세요" />
@@ -8,5 +9,6 @@ export default function Modal() {
 				<button className="modal__button--login">로그인</button>
 				<button className="modal__button--join">회원가입</button>
 				</div>
-		</div>
+	    </div>
+		   </div>
 }
