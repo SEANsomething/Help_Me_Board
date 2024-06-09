6bf275434628219c465f0d3c50014a5b170884d2
이창규 <ichang-gyu@ichang-gyus-MacBook-Pro.local>
2024-06-09 13:24:40 +0900

    html css 정리

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,14 @@
 export default function Modal() {
     return <div className="modal">
-        <div className="modal__popup">
-			<div className="modal__input">
-				<input className="modal__input--id" type ="text" placeholder="ID를 입력하세요" />
-				<input className="modal__input--pw" type="text" placeholder="PW를 입력하세요" />
-		    </div>
-			<div className="modal__button">
-				<button className="modal__button--login">로그인</button>
-				<button className="modal__button--join">회원가입</button>
-				</div>
-	    </div>
+            <div className="modal__popup">
+			  <div className="input">
+				<input className="input_txt" type ="text" placeholder="ID를 입력하세요" />
+				<input className="input_txt" type="text" placeholder="PW를 입력하세요" />
+		      </div>
+			  <div className="button">
+				<button className="button_default">로그인</button>
+				<button className="button_active">회원가입</button>
+			  </div>
+	        </div>
 		   </div>
 }
