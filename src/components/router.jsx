9b52d2894fc584044f65632cd674a062b9fe4c18
SEANsomething <devseanchu@gmail.com>
2024-06-12 20:13:13 +0900

    모달 작업

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -5,15 +5,19 @@ import PostView from "pages/posts/view";
 import PostEdit from "pages/posts/edit";
 import Profile from "pages/profile";
 import SignUp from "pages/signUp";
+import BasicModal from "components/modal/BasicModal";
 
 export default function RouterPage() {
-	return <Routes>
-		<Route path='/' element={<Home />}/>
-		<Route path='/post/new' element={<PostNew />}/>
-		<Route path='/post/:id' element={<PostView />}/>
-		<Route path='/post/edit/:id' element={<PostEdit />}/>
-		<Route path='/profile' element={<Profile />}/>
-		<Route path='/signUp' element={<SignUp />}/>
-		<Route path="*" element={<Navigate replace to="/" />}/>
-	</Routes>
+	return <>
+		{false && <BasicModal></BasicModal>}
+		<Routes>
+			<Route path='/' element={<Home />}/>
+			<Route path='/post/new' element={<PostNew />}/>
+			<Route path='/post/:id' element={<PostView />}/>
+			<Route path='/post/edit/:id' element={<PostEdit />}/>
+			<Route path='/profile' element={<Profile />}/>
+			<Route path='/signUp' element={<SignUp />}/>
+			<Route path="*" element={<Navigate replace to="/" />}/>
+		</Routes>
+	</>
 }
