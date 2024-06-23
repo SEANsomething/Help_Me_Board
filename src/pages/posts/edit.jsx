373202f1b264898e2ef56a23c46f81a84da8ef45
이창규 <ichang-gyu@ichang-gyus-MacBook-Pro.local>
2024-06-23 10:28:29 +0900

    모달이해중

--- src/pages/posts/edit.jsx
+++ src/pages/posts/edit.jsx
@@ -1,10 +1,12 @@
 import Header from "components/Header";
 import PostForm from "components/PostForm";
+import ModalGyu from "components/modal/ModalGyu";
 
 export default function PostEdit() {
 	return <>
 		<Header />
 		<h1>PostEdit Page</h1>
+		{true && <ModalGyu />}
 		<PostForm />
 	</>;
 }
